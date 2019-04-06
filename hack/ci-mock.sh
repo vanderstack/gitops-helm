679fea8170004ace4cdaabf3ad8e20001af73745
vanderstack <github@vanderstack.com>
2019-04-06 11:59:02 -0700

    Update CI Hack to use Google Contianer Repository
    Modified default repository to be GCP podinfo
    Modified docker build and push commands to use GCP
    
    # Remarks
    The repository variable passed in can be formatted using:
    $ export PROJECT_ID=$(gcloud config get-value core/project)
    then it can be invoked using
    $ cd hack && ./ci-mock.sh -r ${PROJECT_ID}/podinfo -b dev

--- hack/ci-mock.sh
+++ hack/ci-mock.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-repository="stefanprodan/podinfo"
+repository="${$(gcloud config get-value core/project)}/podinfo"
 branch="master"
 version=""
 commit=$(cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1 | awk '{print tolower($0)}')
@@ -29,6 +29,6 @@ fi
 
 echo ">>>> Building image ${image} <<<<"
 
-docker build --build-arg GITCOMMIT=${commit} --build-arg VERSION=${version} -t ${image} -f Dockerfile.ci .
+gcloud docker build --build-arg GITCOMMIT=${commit} --build-arg VERSION=${version} -t ${image} -f Dockerfile.ci .
 
-docker push ${image}
+gcloud docker -- push gcr.io/${image}
