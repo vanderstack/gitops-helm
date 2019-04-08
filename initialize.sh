96575952784d556342c79924617ac2d08fcc7dba
vanderstack <github@vanderstack.com>
2019-04-08 12:06:27 -0700

    Documentation improvements

--- initialize.sh
+++ initialize.sh
@@ -11,12 +11,12 @@ kubectl -n kube-system create sa tiller
 kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
 helm init --skip-refresh --upgrade --service-account tiller
 
-# Install Flux Client (unsure if done by helm?)
-# wget https://github.com/weaveworks/flux/releases/download/1.11.1/fluxctl_linux_amd64
-# sudo install -m 755 fluxctl_linux_amd64 /usr/local/bin/fluxctl
 # Set flux namespace
-# export FLUX_FORWARD_NAMESPACE=flux
-# export FLUX_URL=http://127.0.0.1:3030/api/flux
+export FLUX_FORWARD_NAMESPACE=flux
+
+# Install Flux Client
+wget https://github.com/weaveworks/flux/releases/download/1.11.1/fluxctl_linux_amd64
+sudo install -m 755 fluxctl_linux_amd64 /usr/local/bin/fluxctl
 
 # Install Flux Server
 helm repo add weaveworks https://weaveworks.github.io/flux
@@ -24,6 +24,8 @@ helm install --name flux --set rbac.create=true --set helmOperator.create=true -
 
 # Create GitHub Write Access Deploy Key: 
 # Use Browser: https://github.com/vanderstack/gitops-helm/settings/keys/new
+echo ">>>> This is the Github Write Access Deploy Key <<<<"
+echo ">>>> Please update via browser: https://github.com/vanderstack/gitops-helm/settings/keys/new <<<<"
 kubectl -n flux logs deployment/flux | grep identity.pub | cut -d '"' -f2
 
 # Install Sealed Secretes Client
@@ -42,6 +44,12 @@ git stage pub-cert.pem
 git commit -a -m "Add Sealed Secrets public encryption key"
 git push
 
+# Back up Sealed Secrets Private Key
+# kubectl get secret -n adm sealed-secrets-key -o yaml --export > sealed-secrets-key.yaml
+echo ">>>> This is the Private Key used to decrypt Sealed Secrets <<<<"
+echo ">>>> Please make a backup avoid losing access to Sealed Secrets <<<<"
+kubectl get secret -n adm sealed-secrets-key -o yaml --export | cat
+
 # Create example secret
 # kubectl -n dev create secret generic basic-auth --from-literal=user=admin --from-literal=password=admin --dry-run -o json > basic-auth.json
 # kubeseal --format=yaml --cert=pub-cert.pem < basic-auth.json > basic-auth.yaml
@@ -51,12 +59,6 @@ git push
 # git commit -a -m "Add basic auth credentials to dev namespace (example)"
 # git push
 
-# Back up Sealed Secrets Private Key
-# kubectl get secret -n adm sealed-secrets-key -o yaml --export > sealed-secrets-key.yaml
-echo ">>>> This is the Private Key used to decrypt Sealed Secrets <<<<"
-echo ">>>> Please make a backup avoid losing access to Sealed Secrets <<<<"
-kubectl get secret -n adm sealed-secrets-key -o yaml --export | cat
-
 # Restore Sealed Secrets Private Key
 # kubectl replace secret -n adm sealed-secrets-key -f sealed-secrets-key.yaml
 
@@ -65,6 +67,13 @@ kubectl get secret -n adm sealed-secrets-key -o yaml --export | cat
 
 # Trigger a new build
 # cd hack && ./ci-mock.sh -r "podinfo" -b dev
+
+# List all Pods
+# kubectl get pods --all-namespaces -o wide
+
+# Open Shell to pod
+# kubectl exec -it {pod name} -- /bin/bash
+# kubectl exec -it {pod name} -- /bin/sh
 
 # Force Flux Synchronization (default 5 minute wait)
 # fluxctl sync
@@ -83,3 +92,7 @@ kubectl get secret -n adm sealed-secrets-key -o yaml --export | cat
 
 # Turn off Automation
 # fluxctl automate --workload={workload name}
+
+# Review Helm Release History
+# helm history {release name}
+# helm history podinfo-dev
